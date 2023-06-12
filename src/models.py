4c72e87f61a9b47a25ea10fedd38f05d34389b68
talhaseker <talhaseker45@gmail.com>
2023-06-12 03:18:30 -0700

    New models introduced.

--- src/models.py
+++ src/models.py
@@ -1,13 +1,29 @@
 from src.extensions import db
-from flask import jsonify
 
 
-class DummyModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    value = db.Column(db.String, nullable=False)
+class Patient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
-    def json(self) -> str:
-        """
-        :return: Serializes this object to JSON
-        """
-        return jsonify({'id': self.id, 'value': self.value})
+
+class Doctor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+
+class WorkingHours(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    doctor_id = db.Column(db.Integer, db.ForeignKey('doctor.id'), nullable=False)
+    day_of_week = db.Column(db.String(10), nullable=False)
+    start_time = db.Column(db.Time, nullable=False)
+    end_time = db.Column(db.Time, nullable=False)
+
+
+class Appointment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    patient_id = db.Column(db.Integer, db.ForeignKey('patient.id'), nullable=False)
+    doctor_id = db.Column(db.Integer, db.ForeignKey('doctor.id'), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False)
+    end_time = db.Column(db.DateTime, nullable=False)
