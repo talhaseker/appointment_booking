593c23569ac4f456e51041eb73ab7fab07870f7f
talhaseker <talhaseker45@gmail.com>
2023-06-12 03:21:03 -0700

    Schemas logic will be introduced soon.

---
+++ src/schemas.py
@@ -0,0 +1,8 @@
+"""
+
+This is the base module to implement the Marshmallow schemas.
+Marshmallow schemas are extension of Schema class of the Marshmallow library.
+These schemas are basically Data Transfer Object.
+Marshmallow will be used as data validator as well.
+
+"""
